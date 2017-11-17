16f171cea6f6ce3c9f6c979891d8c1bcc2b2396a
Cashfire <maoye.nini@gmail.com>
2017-11-17 15:22:59 -0800

    get all the subsets from a set by recursion

--- src/recurisve/Subsets.java
+++ src/recurisve/Subsets.java
@@ -1,4 +1,7 @@
 package recurisve;
+
+import java.util.ArrayList;
+
 /**
  * return all the subsets of a set.
  * @author Mao
@@ -7,8 +10,35 @@ package recurisve;
 public class Subsets {
 
 	public static void main(String[] args) {
+		ArrayList<String> set = new ArrayList<String>();
+		set.add("a1");
+		set.add("a2");
+		set.add("a3");
+		ArrayList<ArrayList<String>> sb = getSubsets(set, 0);
+		System.out.print(sb);
+		
 		
 
 	}
-
+//	ArrayList<ArrayList<String>>
+	static ArrayList<ArrayList<String>> getSubsets(ArrayList<String> al, int n){
+		ArrayList<ArrayList<String>> allSub; 
+		//end of recursion is 
+		if(al.size()==n){
+			allSub = new ArrayList<ArrayList<String>>();
+			allSub.add(new ArrayList<String>()); //add a empty arraylist
+		}else{
+			allSub = getSubsets(al, n+1);
+			String current= al.get(n);
+			ArrayList<ArrayList<String>> delta = new ArrayList<ArrayList<String>>();
+			for(ArrayList<String> a: allSub){
+				ArrayList<String> newSubset = new ArrayList<String>();
+				newSubset.addAll(a);
+				newSubset.add(current);
+				delta.add(newSubset);
+			}
+			allSub.addAll(delta);
+		}
+		return allSub;
+	}
 }
